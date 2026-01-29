a7f769f9c13d8e3467fde964a850757f0be7644c
ArdynRezky <ardynrezky@upi.edu>
2026-01-29 19:13:19 +0700

    carousel image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Server Actions sudah aktif secara default di Next.js 15
+  // Cukup atur batas body size
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+};
 
 export default nextConfig;
